f9588114b9934499b8d3a0ecc38476895e9d8849
Richie Zed <richie@richezed.com>
2026-05-11 23:42:02 +1000

    Hard coded return value for iPodName

--- lingo-extremote/handler.go
+++ lingo-extremote/handler.go
@@ -75,7 +75,8 @@ func HandleExtRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceExtRemo
 			CmdID:  req.ID.CmdID(),
 		})
 	case *RequestiPodName:
-		ipod.Respond(req, tr, &ReturniPodName{Name: ipod.StringToBytes(dev.Name())})
+		ipod.Respond(req, tr, &ReturniPodName{Name: ipod.StringToBytes("ipod-gadget")})
+		//ipod.Respond(req, tr, &ReturniPodName{Name: ipod.StringToBytes(dev.Name())})
 	case *ResetDBSelection:
 		ipod.Respond(req, tr, ackSuccess(req))
 	case *SelectDBRecord:
